481e8c06175c251d4e2c755f6fff5d77f881ceb2
codeinlife10 <codeinlife10@gmail.com>
2021-06-20 18:27:01 +0700

    Kien thuc buoi 7

--- Day6/README.md
+++ Day6/README.md
@@ -3,6 +3,7 @@ Bài tập:
 
 1. Cho hình chữ nhật có chiều dài = 50m, chiều rộng = 20m. Tính và in ra chu vi, diện tích hcn đó 
 2. In ra màn hình console đoạn text: "Hello + name" trong đó name là giá trị do người dùng nhập vào 
+
 3. Giải pt bậc 2 : ax^2 + bx + c = 0 trong đó a,b,c là giá trị do người dùng nhập vào
 Cho những bạn chưa học trên trường: Tính d = b^2 - 4ac 
 + Nếu d = 0 => Có nghiệm kép x1 = x2 = -b/2a
---
+++ Day7/index.js
@@ -0,0 +1,49 @@
+// Chúng ta sử dụng để xử lí các sự kiện xảy ra 
+// trên trang web 
+/*
+1. Input  => Xử lí dữ liệu này như thế nào 
+*/
+
+// alert("Thong bao toi người dùng")
+
+
+// Là 1 công cụ hữu dụng để debug 
+// console.log(prompt("Enter number 1") * prompt("Enter number 2")) 
+
+// Toán học 
+// Hãy nhận vào 2 input người dùng và thực hiện tính toán 
+// Sinh ra 1 định nghĩa gọi là biến
+
+// let number1 = Number(prompt("Enter number 1")) // prompt cho phép nhập vào input của có kiểu string
+// let number2 = Number(prompt("Enter number 2"))
+
+// console.log(number1 + number2)
+// Giá trị của biến có nhiều loại
+/*
+number 
+string 
+boolean
+
+array 
+object 
+*/
+/*
++ - * / % => / chia lấy phần nguyên , % chia lấy phần dư 
+5/2 = 2 , 5 % 2 = 1 
+*/
+
+// Phân biệt 3 từ khóa let var const sau 
+
+// Number
+let num1 = 5
+// + - * / %
+// string
+// + nối 2 chuỗi vào với nhau 
+let num2 = "5"
+let num3 = '5'
+// boolean kiểm tra điều kiện  
+let check1 = true 
+let check2 = false
+
+let name = prompt("Enter name")
+console.log("Hello" + " " + name)
