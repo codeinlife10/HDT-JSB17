80ee99d3c41be4fb956b88897c2181e6a0d71611
codeinlife10 <codeinlife10@gmail.com>
2021-07-03 18:44:18 +0700

    Các bạn đọc và làm bài tập trong file Readme.md nhé

---
+++ Day9/index.js
@@ -0,0 +1,8 @@
+let human = {
+    name: "Long",
+    favorite: "Playing game, taste new food, enjoy the life",
+    eat: function() {
+        alert("Im eating! Dont disturb me!")
+    },
+}
+human.eat()
