6fb6bee4f19ecd79d5a4467a1d2af6ac5fb3b1f8
codeinlife10 <codeinlife10@gmail.com>
2021-07-17 20:54:49 +0700

    Lam san pham

---
+++ Day11/index.js
@@ -0,0 +1,39 @@
+let user = [
+    { 
+        email:"Long@gmail.com",
+        pass:123456
+    },
+    {
+        // ....
+    }
+    // ....
+]
+
+/* đăng nhập 
+Người dùng nhập vào ô input
+DOM -> innerText -> Dữ liệu nhập vào  
+Duyệt mảng thông qua vòng for -> Chuyển hướng đến trang khác
+-> Thông báo sai và yêu cầu nhập lại
+window.replace, window.redirect -> Nếu thành công thì chuyển hướng 
+-> html,css là chính
+*/
+
+/* Nhóm Hưng/Thiện
+Enter a name ...
+Làm về bóng rổ - quiz
+Giao diện: Thiện 
+Chức năng: Hưng
+Giao diện: https://admin.typeform.com/accounts/01FAT6CGF2QTRVR05BG782X9AK/workspaces/i2WS4f?fbclid=IwAR0ZFK9zvG1cOIeNBfvdetzskYQvdBAqctdgybeiwQEsKw8brbSgBF3oviY
+
+*/
+
+
+/* Nhóm Thành/Minh
+https://themes.shopify.com/themes/loft/styles/lawrence/preview?industry%5B%5D=clothing-fashion&surface_detail=clothing-fashion&surface_inter_position=1&surface_intra_position=19&surface_type=industry
+
+Tuần 1 : 
+Navbar - Minh 
+Hết ShopByType - Thành
+
+
+*/
